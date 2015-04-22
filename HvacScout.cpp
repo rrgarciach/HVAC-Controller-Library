a30e8d9acd368d385f3c9ecc4d6e858af2a6b117
Ruy Garcia <rrgarciach@gmail.com>
2015-04-22 14:32:35 -0500

    master: Added setters and getters.

--- HvacScout.cpp
+++ HvacScout.cpp
@@ -2,28 +2,94 @@
 #include "HvacScout.h"
 #include <SoftwareSerial.h>
 
-HvacScout::HvacScout(String name, uint8_t pinRx, uint8_t pinTx, uint8_t pinKey)
+// constructor:
+HvacScout::HvacScout(String name, uint8_t pinRx, uint8_t pinTx, uint8_t pinKey, uint8_t vcc)
 {
 	// slave Bluetooth socket to communicate with Scout device:
     // For HC-05 Bluetooth module's TX goes to arduino's TX (in this case, pin 3).
-    this->btSerial = new SoftwareSerial(pinRx,pinTx);
+    this->serial = new SoftwareSerial(pinRx,pinTx);
     this->pinKey = pinKey;
+    this->vcc = vcc;
+    pinMode(this->pinKey, OUTPUT);
+    pinMode(this->vcc, OUTPUT);
     this->name = name;
     this->temperature = 0;
     this->power = false;
     this->quiet = false;
-    this->delayTime = 900000; // 15 minutes
+    this->delayTime = 900000; // 15 minutes by default.
 }
-// settings:
+void HvacScout::startSPP()
+{
+    this->serial->begin(38400);
+    Serial.println(F("Starting SPP"));
+    digitalWrite(this->vcc, LOW);
+    digitalWrite(this->pinKey, HIGH);
+    digitalWrite(this->vcc, HIGH);
+    delay(500);
+    this->serial->println("AT");
+    delay(500);
+    this->serial->println("AT+INIT");
+    delay(500);
+    digitalWrite(this->vcc, LOW);
+    delay(500);
+    digitalWrite(this->pinKey, LOW);
+    digitalWrite(this->vcc, HIGH);
+    this->serial->end();
+}
+void HvacScout::start()
+{
+    Serial.println(F("Starting Serial"));
+    this->serial->begin(9600);
+}
+bool HvacScout::readResultFromSerial(String expected, char terminator)
+{
+    if ( this->serial->available() ) {
+        String message;
+        char c;
+        while ( this->serial->available() ) {
+            c = this->serial->read();
+            if (c == terminator) break;
+            message += c;
+        }
+        if (message == expected) {
+            return true;
+        }
+        return false;
+    }
+}
+// setters:
+void HvacScout::setName(String name)
+{
+    this->name = name;
+}
+void HvacScout::setTemperature(uint8_t value)
+{
+    this->temperature = value;
+}
 void HvacScout::setPower(bool newState)
 {
-    this->power = newState;
+    if (newState == true) {
+        this->serial->println("turn_on;");
+        if ( readResultFromSerial("OK",';') ) this->power = true;
+    } else {
+        this->serial->println("turn_off;");
+        if ( readResultFromSerial("OK",';') ) this->power = false;
+    }
 }
-void HvacScout::setDelayTime(uint16_t seconds)
+void HvacScout::setQuiet(bool newState)
 {
-    this->delayTime = (seconds * 1000);
+    this->quiet = newState;
+}
+void HvacScout::setDelayTime(int seconds)
+{
+    this->delayTime = seconds;
+    this->delayTime *= 1000;
 }
 // getters:
+String HvacScout::getName()
+{
+    return this->name;
+}
 uint8_t HvacScout::getTemperature()
 {
     return this->temperature;
@@ -38,5 +104,6 @@ bool HvacScout::getQuiet()
 }
 uint16_t HvacScout::getDelayTime()
 {
-    return (this->delayTime / 1000);
+    int seconds = this->delayTime / 1000;
+    return ( seconds );
 }
--- HvacScout.h
+++ HvacScout.h
@@ -8,23 +8,34 @@ class HvacScout
 {
 	public:
         // constructor:
-		HvacScout(String strName, uint8_t pinRx, uint8_t pinTx, uint8_t pinKey);
+		HvacScout(String strName, uint8_t pinRx, uint8_t pinTx, uint8_t pinKey, uint8_t vcc);
+        void startSPP();
+        void start();
         // setters:
+		void setName(String name);
+		void setTemperature(uint8_t value);
 		void setPower(bool newState);
-		void setDelayTime(uint16_t seconds);
+		void setQuiet(bool newState);
+		void setDelayTime(int seconds);
         // getters:
+		String getName();
 		uint8_t getTemperature();
 		bool getPower();
 		bool getQuiet();
 		uint16_t getDelayTime();
-	private:
-        SoftwareSerial* btSerial;
+        // attributes:
+        SoftwareSerial* serial;
+	protected:
+        // methods:
+        bool readResultFromSerial(String expected, char terminator);
+        // attributes:
         uint8_t pinKey;
+        uint8_t vcc;
         String name;
         uint8_t temperature;
         bool power;
         bool quiet;
-        uint16_t delayTime;
+        uint32_t delayTime;
 };
 
 #endif
